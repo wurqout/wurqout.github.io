d5c906f642cb330458ec8d0ffbbcec731a0b6b86
niall3urke <niall3urke@users.noreply.github.com>
2020-07-09 15:04:14 +0100

    Integrity added but empty

--- service-worker.js
+++ service-worker.js
@@ -17,8 +17,8 @@ async function onInstall(event) {
     // Fetch and cache all matching items from the assets manifest
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)));
-        //.map(asset => new Request(asset.url, { integrity: asset.hash }));
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: "" })); // { integrity: asset.hash }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
